fcd5a76aa8d3fdedb707759f01be458d75ae6d14
Mohit Patni <mohitpatni0731@gmail.com>
2024-12-25 19:17:40 -0800

    Update climbing_stairs.py

--- DP/climbing_stairs.py
+++ DP/climbing_stairs.py
@@ -35,3 +35,15 @@ class Solution:
         for i in range(2, n+1): 
             DP[i] = DP[i-1] + DP[i-2]
         return DP[-2]
+
+
+## Constant space solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        prev = 1
+        curr = 2
+        for i in range(2, n): 
+            prev, curr = curr, prev+curr
+        return curr
