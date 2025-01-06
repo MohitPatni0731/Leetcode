6c34482e23853a56be15e2270be1b5fc41af0796
Mohit Patni <mohitpatni0731@gmail.com>
2025-01-05 23:18:22 -0800

    Update partition_equal_subset_sum.py

--- DP/partition_equal_subset_sum.py
+++ DP/partition_equal_subset_sum.py
@@ -9,4 +9,17 @@ class Solution:
         else:
             return False
 
-# DP Appraoch - 
+# DP Appraoch - Naive recursive
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        if total_sum/2 != total_sum//2:
+            return False
+        
+        def dp(i, amount):
+            if i >= len(nums) or amount >= total_sum/2:
+                return amount == total_sum / 2
+            
+            return dp(i+1, amount + nums[i]) or dp(i+1, amount)
+        return dp(0,0)
+        
